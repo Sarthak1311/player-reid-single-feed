b61c2ba4d8eae67cc7c2d2a069b5a0fbc75a5710
Sarthak Tyagi <113363652+Sarthak1311@users.noreply.github.com>
2025-07-21 23:33:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-#Import All the Required Libraries
 from utils import read_video, save_video
 from trackers import Tracker
 def main():
-    #Read Video
-    video_frames = read_video("/Users/sarthaktyagi/Downloads/Football-Analysis-System-Using-YOLO11-main/15sec_input_720p.mp4")
+    #Reading Video
+    video_frames = read_video("/Users/sarthaktyagi/Desktop/internshipProject/15sec_input_720p.mp4")
 
     #Initialize Tracker
-    tracker = Tracker("/Users/sarthaktyagi/Downloads/Football-Analysis-System-Using-YOLO11-main/model/best.pt")
+    tracker = Tracker("/Users/sarthaktyagi/Desktop/internshipProject/best.pt")
     tracks = tracker.get_object_tracks(video_frames, read_from_stub=False, stub_path='tracker_stubs/player_detection.pkl')
-
-    #Draw Output
     #Draw Object Tracks
     output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
-    #Save Video
+    #Save
     save_video(output_video_frames, 'output_videos/output.mp4')
 
 
